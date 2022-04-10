2fd9a6dfc7cca19feca3cdafd20d0dc4cd7656c9
lsh <rrgks@naver.com>
2022-04-10 16:20:08 +0900

    Modify:   1. 환경변수 베이터베이스 관련 수정   2. 데이터베이스 config 수정

--- app/src/config/mairadb.js
+++ app/src/config/mairadb.js
@@ -5,9 +5,8 @@ const db = require('mariadb');
 const mariadb = db.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  dateStrings: 'date',
   port: 3307,
   connectionLimit: 5,
 });
