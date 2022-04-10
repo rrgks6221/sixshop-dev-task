333d82965dd69cbda918758ef88864e67ed182b3
lsh <rrgks@naver.com>
2022-04-10 19:19:50 +0900

    Modify: processCtrl if문 조건 수정 #2

--- app/src/utils/processCtrl.js
+++ app/src/utils/processCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (res, response) => {
-  if (response.status === undefined) {
+  if (response.isError) {
     return res.status(500).json(response.clientMsg);
   }
   return res.status(response.status).json(response);
