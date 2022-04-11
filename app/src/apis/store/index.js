d5f2bfe6eea38b9ecc122854b6024a4ecd1c5293
lsh <rrgks@naver.com>
2022-04-12 00:32:30 +0900

    Modify: 상점 필수 값 조회 endpoint 수정

--- app/src/apis/store/index.js
+++ app/src/apis/store/index.js
@@ -5,7 +5,7 @@ const ctrl = require('./store.ctrl');
 
 const router = express.Router();
 
-router.get('/:storeName/sign-up', ctrl.findOneFlagByName);
+router.get('/:storeName/essential', ctrl.findOneFlagByName);
 
 router.post('/:storeName/sign-up', ctrl.signUp);
 
